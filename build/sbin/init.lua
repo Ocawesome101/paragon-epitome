c09e953ff6851357322a94bfbf369f9996231d50
Ocawesome101 <ocawesome101@gmail.com>
2020-09-19 00:17:54 -0400

    few more libraries, bit more protection

--- build/sbin/init.lua
+++ build/sbin/init.lua
@@ -203,15 +203,16 @@ do
   io.write("\27[92m*\27[97m Adding kernel APIs....")
   local k = k
   _G.k = nil
-  package.loaded.sha3 = k.sha3
-  package.loaded.sha2 = k.sha2
-  package.loaded.ec25519 = k.ec25519
+  package.loaded.sha3 = package.protect(k.sha3)
+  package.loaded.sha2 = package.protect(k.sha2)
+  package.loaded.ec25519 = package.protect(k.ec25519)
   package.loaded.uuid = k.uuid
   package.loaded.minitel = k.drv.net.minitel
   package.loaded.gert = k.drv.net.gert
   package.loaded.event = k.evt
   package.loaded.vt100 = k.vt
   package.loaded.hostname = k.hostname
+  package.loaded.users = package.protect(k.security.users)
   io.write("Done.\n")
 end
 
