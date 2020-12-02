a8767bfe6fe44d31d9d878b995e9189d1032a0f8
Ocawesome101 <ocawesome101@gmail.com>
2020-12-02 02:00:45 -0500

    more progress on scripts

--- src/scripts.lua
+++ src/scripts.lua
@@ -2,4 +2,10 @@
 
 do
   local script_path = "/etc/epitome/scripts"
+  local fs = require("filesystem")
+
+  local files = fs.list(script_path)
+  table.sort(files)
+  for i=1, #files, 1 do
+  end
 end
